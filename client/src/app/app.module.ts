0d352ef3eeb8b01106d7e0ad36ef5a10adec4f27
Alper Silistre <alpersilistre@gmail.com>
2018-10-29 19:33:17 +0300

    Initial project structure, login page design implemented.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RootComponent } from './root/root.component';
+
+import { ROUTING } from './app.routing';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    RootComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    ROUTING
+  ],
+  providers: [],
+  bootstrap: [RootComponent]
+})
+export class AppModule { }
